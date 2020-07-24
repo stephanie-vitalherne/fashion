3603fd3b0b3181db3f8f7860f7094e45532a3fe3
Stephanie Vital-Herne <vitalherne@gmail.com>
2020-07-24 00:43:50 -0400

    got a sliding content scroll going

--- src/Authentication/Onboarding/Onboarding.tsx
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -1,10 +1,73 @@
 import React from "react";
-import { View } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
+import Animated from "react-native-reanimated";
+import { useValue, onScrollEvent, interpolateColor } from "react-native-redash";
 
-interface OnboardingProps {}
+// COMPONENTS
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+
+const BORDER_RADIUS = 75;
+const { width } = Dimensions.get("window");
+
+const slides = [
+  { label: "Relaxed", color: "#BFEAF5" },
+  { label: "Playful", color: "#BEECC4" },
+  { label: "Eccentric", color: "#FFE4D9" },
+  { label: "Funky", color: "#FFDDDD" },
+];
 
 const Onboarding = () => {
-  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+  const x = useValue(0);
+  const onScroll = onScrollEvent({ x });
+  const backgroundColor = interpolateColor(x, {
+    inputRange: slides.map((_, i) => i * width),
+    outputRange: slides.map((slide) => slide.color),
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.slider, { backgroundColor }]}>
+        <Animated.ScrollView
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          scrollEventThrottle={1}
+          {...{ onScroll }}
+        >
+          {slides.map(({ label }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ label }} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <Animated.View
+          style={{ ...StyleSheet.absoluteFillObject, backgroundColor }}
+        />
+        <View style={styles.footerContent}></View>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  slider: {
+    height: SLIDE_HEIGHT,
+    borderBottomRightRadius: BORDER_RADIUS,
+  },
+  footer: {
+    flex: 1,
+  },
+  footerContent: {
+    flex: 1,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
+});
 
 export default Onboarding;
